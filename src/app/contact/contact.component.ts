de294d3e1bb82bc6303648eab765db57580a7c6a
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2024-08-13 01:44:03 +0300

    Footer and contacts update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
