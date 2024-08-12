c41bff18690d58d43772fc989bb267a8a2098b42
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2024-08-13 01:28:47 +0300

    Footer and contacts update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,12 @@ import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
 import { ProjectsComponent } from '../projects/projects.component';
 import { TestimonialsComponent } from '../testimonials/testimonials.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,CarouselModule,ButtonModule,TagModule,ProjectsComponent,TestimonialsComponent],
+  imports: [CommonModule,CarouselModule,ButtonModule,TagModule,ProjectsComponent,TestimonialsComponent,ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
