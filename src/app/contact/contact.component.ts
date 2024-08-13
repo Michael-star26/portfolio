c614e22055afc1b68486173743c46ff124837128
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2024-08-13 05:03:25 +0300

    Footer and contacts update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { ScrollTopModule } from 'primeng/scrolltop';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [RouterOutlet,RouterLink,RouterModule],
+  imports: [RouterOutlet,RouterLink,RouterModule,ScrollTopModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ScrollTopModule } from 'primeng/scrolltop';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ScrollTopModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
