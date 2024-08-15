225dddb6067ee8c3f57919b22127041a9d261ec9
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2024-08-15 03:34:38 +0300

    Added real data to carousel and updated angular projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { TestimonialsComponent } from '../testimonials/testimonials.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [TestimonialsComponent],
+  imports: [TestimonialsComponent,CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-
+  projects={
+    "E-commerce":"www.ecomm",
+    "Reactive Form":"www.ecomm",
+    "Angular States":"www.ecomm",
+    "Todo App":"www.ecomm",
+    "Sport Store":"www.ecomm",
+    "Youtube video":"www.ecomm",
+    "Music app":"www.ecomm",
+    "Reddit clawn":"www.ecomm",
+  }
 }
